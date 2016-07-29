b388f9b54db40012fe231748a720dc9ea71d7308
Aya <aya@bienemann.me>
2016-07-29 17:39:39 -0300

    chnging app transport security settings

---
+++ sptransAPI/Helpers/SPTransAPI.swift
@@ -0,0 +1,17 @@
+//
+//  SPTransAPI.swift
+//  sptransAPI
+//
+//  Created by resource on 7/29/16.
+//  Copyright © 2016 bienemann. All rights reserved.
+//
+
+import Foundation
+
+class SPTransAPI : NSObject {
+    
+    func authenticate() {
+        
+    }
+    
+}
