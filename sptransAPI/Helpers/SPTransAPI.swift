7930f58ca19f3ac091565ad7c39d1fe8d6cc536c
Aya <aya@bienemann.me>
2016-07-29 17:56:24 -0300

    creating api singleton and authentication endpoint

--- sptransAPI/Helpers/SPTransAPI.swift
+++ sptransAPI/Helpers/SPTransAPI.swift
@@ -7,11 +7,19 @@
 //
 
 import Foundation
+import Alamofire
 
 class SPTransAPI : NSObject {
     
+    static let shared = SPTransAPI()
+    let apiKey = "e86972bad776dfaaba882db93230bf1b4745a4c9d21ddfcd15c71106d2fa6f79"
+    let baseURL = "http://api.olhovivo.sptrans.com.br/v0"
+    
     func authenticate() {
-        
+        let requestURL = "\(baseURL)/Login/Autenticar?token=\(apiKey)"
+        Alamofire.request(.POST, requestURL).responseJSON { response in
+            print(response);
+        }
     }
     
 }
--- sptransAPI/ViewController.swift
+++ sptransAPI/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        SPTransAPI.shared.authenticate()
+        
     }
 
     override func didReceiveMemoryWarning() {
