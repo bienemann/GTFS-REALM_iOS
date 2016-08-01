6831cead2414955373d3107da61b384b31e2ace3
Aya <aya@bienemann.me>
2016-08-01 17:16:20 -0300

    crating Realm models for agency and route

---
+++ sptransAPI/Models/GTFS Model/GTFSAgency.swift
@@ -0,0 +1,28 @@
+//
+//  GTFSAgency.swift
+//  sptransAPI
+//
+//  Created by resource on 8/1/16.
+//  Copyright © 2016 bienemann. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class GTFSAgency: Object {
+    
+    dynamic var agency_id : Int = 0
+    dynamic var agency_name : String = ""
+    dynamic var agency_url : String = ""
+    dynamic var agency_timezone : String = ""
+    let agency_lang : String? = nil
+    let agency_phone : String? = nil
+    let agency_fare_url : String? = nil
+    let agency_email : String? = nil
+    
+// Specify properties to ignore (Realm won't persist these)
+    
+//  override static func ignoredProperties() -> [String] {
+//    return []
+//  }
+}
---
+++ sptransAPI/Models/GTFS Model/GTFSRoute.swift
@@ -0,0 +1,30 @@
+//
+//  GTFSRoute.swift
+//  sptransAPI
+//
+//  Created by resource on 8/1/16.
+//  Copyright © 2016 bienemann. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class GTFSRoute: Object {
+    
+    dynamic var route_id : Int = 0
+    dynamic var agency_id : GTFSAgency?
+    dynamic var route_short_name : String = ""
+    dynamic var route_long_name : String = ""
+    let route_desc : String? = nil
+    dynamic var route_type : Int = 999
+    let route_url : String? = nil
+    let route_color : String? = nil
+    let route_text_color : String? = nil
+    
+    
+// Specify properties to ignore (Realm won't persist these)
+    
+//  override static func ignoredProperties() -> [String] {
+//    return []
+//  }
+}
