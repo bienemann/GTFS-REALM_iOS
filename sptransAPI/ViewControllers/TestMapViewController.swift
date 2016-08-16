466f72f653f06a8171f6d5fcf9485012cfb348b2
Allan <allan.martins@resource.com.br>
2016-08-16 13:17:05 -0300

    drawing line route on map

--- sptransAPI/ViewControllers/TestMapViewController.swift
+++ sptransAPI/ViewControllers/TestMapViewController.swift
@@ -13,16 +13,46 @@ class TestMapViewController: UIViewController, MKMapViewDelegate {
     
     @IBOutlet weak var mapView : MKMapView?
     var line : GTFSTripPolyline? = nil
+    var lineRenderer : MKPolylineRenderer?
     
-    override func viewDidLoad() {
-        self.plotLine(self.line!)
+    override func viewDidAppear(animated: Bool) {
+        if self.line != nil{
+            self.lineRenderer = self.customLineRenderer(self.line!)
+            self.mapView!.addOverlay(self.lineRenderer!.overlay)
+            self.mapView!.setVisibleMapRect(self.mapRectWithBorder(self.lineOverlay()!), animated: true)
+        }
     }
     
-    func plotLine(line: GTFSTripPolyline){
+    func lineOverlay() -> MKOverlay?{
+        if self.lineRenderer != nil {
+            return self.lineRenderer!.overlay
+        }else{
+            return nil
+        }
+    }
+    
+    func mapRectWithBorder(overlay: MKOverlay) -> MKMapRect{
+        let mapRect = overlay.boundingMapRect
+        return MKMapRectMake(mapRect.origin.x - mapRect.size.width/20,
+                             mapRect.origin.y - mapRect.size.height/20,
+                             mapRect.size.width + mapRect.size.width/10,
+                             mapRect.size.height + mapRect.size.height/10)
+    }
+    
+    func customLineRenderer(line: GTFSTripPolyline) -> MKPolylineRenderer{
         let renderer = MKPolylineRenderer(polyline: line)
-        self.mapView?.addOverlay(renderer.overlay)
-        self.mapView?.regionThatFits(MKCoordinateRegion(
-            center: line.coordinate, span: MKCoordinateSpan(latitudeDelta: 90, longitudeDelta: 90)))
+        renderer.strokeColor = UIColor.redColor()
+        renderer.lineWidth = 5
+        return renderer
+    }
+    
+    //MKMapView Delegate
+    
+    func mapView(mapView: MKMapView, rendererForOverlay overlay: MKOverlay) -> MKOverlayRenderer {
+        if overlay.isEqual(self.lineRenderer!.overlay)  {
+            return self.lineRenderer!
+        }
+        return MKOverlayRenderer()
     }
     
 }
